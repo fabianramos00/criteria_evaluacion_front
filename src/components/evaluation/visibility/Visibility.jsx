1f07fa5e73aafaaf1b61647a10b4a7665a23ba73
CamHM <cami.andre469@gmail.com>
2021-08-06 06:07:25 -0500

    components refactor

--- src/components/evaluation/visibility/Visibility.jsx
+++ src/components/evaluation/visibility/Visibility.jsx
@@ -1,4 +1,5 @@
 import '../style.scss';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { INVALID_URL_ERROR } from '../../../const/errors';
@@ -6,6 +7,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { StepControls } from '../../steps/Steps';
 import { policiesRoute } from '../../../const/routes';
 import { useParams } from 'react-router-dom';
+import RadioGroup from '../../general/radioGroup/RadioGroup';
 
 const schema = yup.object().shape({
   repository_url: yup.string().url(INVALID_URL_ERROR),
@@ -16,6 +18,8 @@ const schema = yup.object().shape({
 });
 
 const Visibility = () => {
+  const [national, setNational] = useState(0);
+  const [showNext, setShowNext] = useState(false);
   const {
     handleSubmit,
     register,
@@ -28,34 +32,90 @@ const Visibility = () => {
   };
 
   return (
-    <>
-      <h4>(opcional) Ingresa la url de hasta cinco recolectores nacionales.</h4>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='visibility__form'>
-          <div>
-            <input {...register('repository_url')} placeholder='Repositorio nacional 1' />
-            <p className='form__error'>{errors.repository_url?.message}</p>
-          </div>
-          <div>
-            <input {...register('repository_url1')} placeholder='Repositorio nacional 2' />
-            <p className='form__error'>{errors.repository_url1?.message}</p>
-          </div>
-          <div>
-            <input {...register('repository_url2')} placeholder='Repositorio nacional 3' />
-            <p className='form__error'>{errors.repository_url2?.message}</p>
-          </div>
-          <div>
-            <input {...register('repository_url3')} placeholder='Repositorio nacional 4' />
-            <p className='form__error'>{errors.repository_url3?.message}</p>
-          </div>
-          <div>
-            <input {...register('repository_url4')} placeholder='Repositorio nacional 5' />
-            <p className='form__error'>{errors.repository_url4?.message}</p>
-          </div>
-        </div>
-        <StepControls showBack={false} nextRoute={policiesRoute(token)} />
-      </form>
-    </>
+    <div className='visibility'>
+      <div className='option'>
+        <p>
+          <b>1. Presencia en directorios internacionales:</b> OpenDOAR, ROAR, OAI Data Providers,
+          e3dat
+        </p>
+      </div>
+      <div className='option'>
+        <p>
+          <b>2. Presencia en recolectores internacionales:</b> LA Referencia, OpenAIRE, Google
+          Académico, CORE, BASE
+        </p>
+      </div>
+      <div className='option'>
+        <p>
+          <b>3. Presencia en recolectores nacionales.</b>
+        </p>
+        <RadioGroup
+          text=''
+          options={[
+            { id: 1, label: 'Sí', value: 3 },
+            { id: 2, label: 'No', value: 0 },
+          ]}
+          onChange={setNational}
+        />
+        {national > 0 && (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='visibility__form'>
+              <div>
+                <input {...register('repository_url')} placeholder='Repositorio nacional 1' />
+                <p className='form__error'>{errors.repository_url?.message}</p>
+              </div>
+              <div>
+                <input {...register('repository_url1')} placeholder='Repositorio nacional 2' />
+                <p className='form__error'>{errors.repository_url1?.message}</p>
+              </div>
+              <div>
+                <input {...register('repository_url2')} placeholder='Repositorio nacional 3' />
+                <p className='form__error'>{errors.repository_url2?.message}</p>
+              </div>
+              <div>
+                <input {...register('repository_url3')} placeholder='Repositorio nacional 4' />
+                <p className='form__error'>{errors.repository_url3?.message}</p>
+              </div>
+              <div>
+                <input {...register('repository_url4')} placeholder='Repositorio nacional 5' />
+                <p className='form__error'>{errors.repository_url4?.message}</p>
+              </div>
+            </div>
+          </form>
+        )}
+      </div>
+      <div className='option'>
+        <p>
+          <b>4. Existencia de nombre normalizado del RI en directorios y recolectores.</b>
+        </p>
+      </div>
+      <div className='option'>
+        <p>
+          <b>5. Existencia de URL segura (https) y amigable (nombre del RI).</b>
+        </p>
+      </div>
+      <div className='option'>
+        <p>
+          <b>6. Disponibilidad de documentos en acceso abierto.</b>
+        </p>
+      </div>
+      <div className='option'>
+        <p>
+          <b>
+            7. Existencia de iniciativas para fomentar la visibilidad del repositorio dentro de la
+            propia institución.
+          </b>
+        </p>
+        <RadioGroup
+          text=''
+          options={[
+            { id: 1, label: 'Sí', value: 3 },
+            { id: 2, label: 'No', value: 0 },
+          ]}
+        />
+      </div>
+      <StepControls showBack={false} nextRoute={policiesRoute(token)} nextText={showNext} />
+    </div>
   );
 };
 
--- src/components/steps/Steps.jsx
+++ src/components/steps/Steps.jsx
@@ -1,36 +1,64 @@
 import './Steps.scss';
+import { useState, useMemo } from 'react';
 import { Switch, Route, useRouteMatch, NavLink, useParams, useHistory } from 'react-router-dom';
 
 function Steps({ items = [] }) {
+  const [headerPage, setHeaderPage] = useState(0);
   const { path } = useRouteMatch();
   const { token } = useParams();
+
+  const headerSteps = useMemo(() => items.slice(headerPage, headerPage + 3), [headerPage, items]);
+  const hasNext = useMemo(() => headerPage + 4 < items.length, [headerPage, items]);
+  const hasPrev = headerPage > 0;
 
   const getPath = (stepPath, token) => {
     return typeof stepPath === 'function' ? stepPath(token) : `${path}${stepPath}`;
   };
 
+  const next = () => {
+    if (hasNext) setHeaderPage(headerPage + 1);
+  };
+
+  const prev = () => {
+    if (hasPrev) setHeaderPage(headerPage - 1);
+  };
+
   return (
     <div className='steps'>
-      {items.map((step, i) => (
-        <div key={`step-${step.id}`} className='steps__item'>
-          <div className='steps__header'>
-            <NavLink
-              to={getPath(step.path, token)}
-              activeClassName='steps__badge steps__badge--complete'
-              className='steps__badge'
-              exact
-            >
-              <div>{i + 1}</div>
-            </NavLink>
-            <div className='steps__label'>{step.label}</div>
+      <div className='header'>
+        <i
+          className={`fas fa-chevron-circle-left pagination-btn ${!hasPrev && 'hide'}`}
+          onClick={prev}
+        />
+        {headerSteps.map((step, i) => (
+          <div key={`step-${step.id}`} className='item'>
+            <div className='steps__header'>
+              <NavLink
+                to={getPath(step.path, token)}
+                activeClassName='steps__badge steps__badge--complete'
+                className='steps__badge'
+                exact
+              >
+                <div>{headerPage + i + 1}</div>
+              </NavLink>
+              <div className='label'>{step.label}</div>
+            </div>
           </div>
-          <Switch>
+        ))}
+        <i
+          className={`fas fa-chevron-circle-right pagination-btn ${!hasNext && 'hide'}`}
+          onClick={next}
+        />
+      </div>
+      <div className='content'>
+        {items.map(step => (
+          <Switch key={`content-${step.id}`}>
             <Route exact path={getPath(step.path)}>
               <Step step={step} />
             </Route>
           </Switch>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
@@ -39,7 +67,12 @@ const Step = ({ step }) => {
   return <div className='steps__content'>{step.component}</div>;
 };
 
-export const StepControls = ({ showBack = true, nextRoute = '', backRoute = '' }) => {
+export const StepControls = ({
+  showBack = true,
+  nextRoute = '',
+  backRoute = '',
+  nextText = false,
+}) => {
   const history = useHistory();
 
   const goNext = () => nextRoute && history.push(nextRoute);
@@ -55,10 +88,10 @@ export const StepControls = ({ showBack = true, nextRoute = '', backRoute = '' }
       )}
       <button
         type='submit'
-        onClick={goNext}
+        onClick={nextText && goNext}
         className='step-controls__btn step-controls__btn--next'
       >
-        Siguiente
+        {nextText ? 'Siguiente' : 'Guardar'}
       </button>
     </div>
   );
